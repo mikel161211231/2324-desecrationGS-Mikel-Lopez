b21ac891b3d77047c2f5985b200015f2a4d1a9c1
161211231 <mikel.lopez@ikasle.aeg.eus>
2024-06-05 10:02:36 +0200

    [UPDATE] Create the roll method for the die.

--- src/die.mjs
+++ src/die.mjs
@@ -11,12 +11,21 @@ export default class Die {
 
     static createDie(num, id){
         const values = [];
-
         for (let i = 1; i < (num +1); i++) {
             values.push(i);
         }
 
         return (new Die(values, id));
+    }
+
+    roll(){
+        let result = -1;
+        let position = -1;
+        
+        position = Math.floor(Math.random()*this.values.length);
+        result = this.values[position];
+
+        return result;
     }
 
 }
