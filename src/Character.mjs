68144a9ce673c72c9c82afd1e1ed8ec85f05c63b
161211231 <mikel.lopez@ikasle.aeg.eus>
2024-06-05 10:10:54 +0200

    [NEW & UDPATE] Create the class Character and the empty method attack.

---
+++ src/Character.mjs
@@ -0,0 +1,22 @@
+
+
+
+export default class Character {
+
+    constructor(name, int, str, dur, spe, pow, com, hp){
+
+        this.name = name; // Nombre del personaje
+
+        this.int = int; // Inteligencia
+        this.str = str; // Fuerza
+        this.dur = dur; // Durabilidad
+        this.spe = spe; // Velocidad
+        this.pow = pow; // Poder
+        this.com = com; // Combate 
+        this.hp = hp; // Vida
+    }
+
+    attack(){
+        // Vacio, las clases que lo hereden tendran el contenido
+    }
+}
